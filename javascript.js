bc5f3aee5c87574c9306ddbf32e61cb5fc562726
drolcak <34201809+drolcak@users.noreply.github.com>
2018-04-16 21:27:00 +0200

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+
+function JakaCyfra() //przypisanie do przycisków "cyferek"
+
+{
+    let temp = ""; //tymczasowa zmienna
+
+    for (i=1; i<=10; i++)
+   {
+       temp = temp + i;
+       //var div = document.createElement("div"); //tworzenie kolejnych divów
+        // div.innerHTML = i;
+       // document.getElementById("results").appendChild(div); //dodanie do diva results
+   }
+
+     // document.getElementById("activities").innerText = "wypisano: " + temp;
+return temp;
+}
